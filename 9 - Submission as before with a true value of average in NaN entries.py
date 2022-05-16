d0fa82a9c7988d7dd1fa1ee61b582870a3cb8717
JonatanPlantey <jonatan.plantey@sfr.fr>
2022-05-16 11:38:28 +0200

    Update
    Update

--- 9 - Submission as before with a true value of average in NaN entries.py
+++ 9 - Submission as before with a true value of average in NaN entries.py
@@ -16,8 +16,7 @@ def weighted_average_for_string_column(Series):
     Series_value_counts = Series_value_counts.reset_index(drop = True)
     sum_prod = 0
     for i in range(len(Series_value_counts)):
-        #sum_prod += Series_value_counts.iloc[i] * i
-        sum_prod += Series_value_counts.iloc[i] * (i + 1)
+        sum_prod += Series_value_counts.iloc[i] * i
     weighted_average = sum_prod / sum(Series_value_counts)
     return weighted_average
 
@@ -63,29 +62,30 @@ string_features_selected = [column_name for column_name in features_selected if 
 print(string_features_selected)
 # Function to convert columns in a DataFrame who contain string values using directories and save these
 # For each use, this one save dictionary used into the list below in order to reuse further
-list_of_dict_used = []
-def convert_to_numbers_and_save_dict_used(column_name, DataFrame, list_of_dict):
-    # Copy the column without NaN entries before changes to calculate the weighted average later
-    column_without_NaN_entries = DataFrame.loc[:, column_name].dropna()
-    # Fill "NaN" entries with "Unknown"
-    DataFrame.loc[:, column_name] = DataFrame.loc[:, column_name].fillna("Unknown")
-    column_value_counts = DataFrame.loc[:, column_name].value_counts()
-    #print(column_value_counts)
+dicts = {}
+def convert_to_numbers_and_save_dict(column_name, DataFrame, dicts):
+    # Copy the column without NaN entries and calculate the weighted average
+    column_without_NaN_entries = DataFrame.loc[:, column_name].dropna(axis = 0)
+    column_value_counts = column_without_NaN_entries.value_counts()
+    weighted_average_for_str_column = weighted_average_for_string_column(column_without_NaN_entries) * 1
     # Warning. We have to convert column_value_counts_index into list to find the place of each string value in its index:
     column_value_counts_index = column_value_counts.index.tolist()
     column_dict = {string_value: column_value_counts_index.index(string_value) for string_value in column_value_counts_index}
     # We assure the key "Unknown" is in dictionary to avoid empty entries problems with test_data during submission
     # We give to the "Unknown" key the value of weighted average of values in the new dictionary
     print(column_dict)
-    # Calculate the weighted average
-    column_dict["Unknown"] = weighted_average_for_string_column(column_without_NaN_entries) * 1
+    # Fill "NaN" entries with "Unknown"
+    DataFrame.loc[:, column_name] = DataFrame.loc[:, column_name].fillna("Unknown")
+    # Associate "Unknown" key with the weighted average value in column_dict
+    column_dict["Unknown"] = weighted_average_for_str_column
     print(column_dict)
     DataFrame.loc[:, column_name] = DataFrame.loc[:, column_name].map(lambda p: column_dict[p])
-    list_of_dict = list_of_dict.append(column_dict)
+    dicts[column_name] = column_dict
 
 # Convert columns
 for column_name in string_features_selected:
-    convert_to_numbers_and_save_dict_used(column_name, X, list_of_dict_used)
+    convert_to_numbers_and_save_dict(column_name, X, dicts)
+print(dicts)
 
 # Split X and y into validation and training data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
@@ -98,16 +98,16 @@ default_val_mae = mean_absolute_error(model_default_val_preds, val_y)
 print("\nValidation MAE for Model with no value of max_leaf_node: {:,.0f}\n".format(default_val_mae))
 
 # Functions used
-# Function to convert a string columns with a new 'list_of_dict_used'
-def convert_string_column_into_numbers_using_dict_in_list_of_dict_used(column_name, DataFrame, dict):
+# Function to convert a string columns with a new 'dicts'
+def convert_string_column_into_numbers_using_dict_in_dicts(column_name, DataFrame, dict):
     # Fill "NaN" entries with "Unknown"
     DataFrame.loc[:, column_name] = DataFrame.loc[:, column_name].fillna("Unknown")
     DataFrame.loc[:, column_name] = DataFrame.loc[:, column_name].map(lambda p: dict[p])
 # Function to convert all string columns with a new 'list_of_dict_used'
-def convert_string_columns_into_numbers_using_list_of_dict_used(X, list_of_dict_used):
+def convert_string_columns_into_numbers_using_dicts(X, dicts):
     i = 0
     for column_name in string_features_selected:
-        convert_string_column_into_numbers_using_dict_in_list_of_dict_used(column_name, X, list_of_dict_used[i])
+        convert_string_column_into_numbers_using_dict_in_dicts(column_name, X, dicts[column_name])
         i += 1
 
 # Fit Model Using All Data
@@ -120,7 +120,7 @@ test_data = pd.read_csv(test_path)
 test_X = test_data[features_selected]
 # Fill and convert columns selected as before
 fill_NaN_by_average_in_all_numbers_columns(test_X, number_features_selected)
-convert_string_columns_into_numbers_using_list_of_dict_used(test_X, list_of_dict_used)
+convert_string_columns_into_numbers_using_dicts(test_X, dicts)
 print(test_X.head())
 # make predictions which we will submit.
 test_preds = final_model.predict(test_X)
